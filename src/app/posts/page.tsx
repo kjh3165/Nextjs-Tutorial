715d97687439e77d6de4866147ee75dd8ff83a7b
kjh3165 <jhwk109@gmail.com>
2026-03-23 10:48:31 +0900

    Step 12

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,14 +4,19 @@ import { supabase } from '@/supabase/supabase'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
+interface Post {
+  id: number
+  created_at: string
+  title: string
+  content: string
+}
+
 export default function Posts() {
-  const [posts, setPosts] = useState<
-    Array<{ id: number; title: string; content: string }>
-  >([])
+  const [posts, setPosts] = useState<Post[]>([])
 
   const fetchData = async () => {
     const { data: posts, error } = await supabase.from('posts').select('*')
-    setPosts(posts ?? [])
+    setPosts((posts as Post[]) ?? [])
   }
 
   useEffect(() => {
