2f49a901715fce3328c4ffdcd77ac6fd0d2a9a04
kjh3165 <jhwk109@gmail.com>
2026-03-24 12:42:14 +0900

    Step 26

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -22,6 +22,7 @@ export default function PostDetail() {
   const { id } = useParams()
   const [post, setPost] = useState<Post | null>(null)
   const [comments, setComments] = useState<Comment[]>([])
+  const [comment, setComment] = useState<string>('')
   const router = useRouter()
 
   const fetchPost = async () => {
@@ -59,11 +60,39 @@ export default function PostDetail() {
       router.push('/posts')
     }
   }
+
+  const handleOnSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const { error } = await supabase
+      .from('comments')
+      .insert([{ post_id: id, content: comment }])
+      .select()
+    if (error) {
+      alert(error.message)
+    } else {
+      alert('댓글 작성 성공!')
+      setComment('')
+      fetchComments()
+    }
+  }
+
   return (
     <>
       <div>{id}번 게시글 상세</div>
       <div>{post.title}</div>
       <div>{post.content}</div>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          name="comment"
+          value={comment}
+          placeholder="댓글 입력"
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button className="p-2 rounded border-2 border-gray-200">
+          댓글 작성
+        </button>
+      </form>
       <ul>
         {comments?.map((comment) => (
           <li key={comment.id}>- {comment.content}</li>
